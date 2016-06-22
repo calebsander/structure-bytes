cf2a851667734257cc6de3ce2765d0851d53e780
Caleb Sander <caleb.sander@gmail.com>
2016-06-21 22:39:22 -0400

    Fixed indentation; moved asserts to their own file; using growable arrays for construction

--- structure-types.js
+++ structure-types.js
@@ -1,55 +1,60 @@
+const assert = require(__dirname + '/lib/assert.js');
 const GrowableBuffer = require(__dirname + '/lib/growable-buffer.js');
 
 class Type {
-  static get _value() {
-    throw new Error('Generic Type has no value byte');
-  }
-  toBuffer() {
-    if (!this.cachedBuffer) this.cachedBuffer = this._toBuffer();
-    return this.cachedBuffer;
-  }
-  _toBuffer() {
-    let buffer = Buffer.allocUnsafe(1);
-    buffer[0] = this.constructor._value;
-    return buffer;
-  }
+	static get _value() {
+		throw new Error('Generic Type has no value byte');
+	}
+	addToBuffer(buffer) {
+		assert.instanceOf(buffer, GrowableBuffer);
+		buffer.add(this.constructor._value);
+	}
+	toBuffer() {
+		if (!this.cachedBuffer) this.cachedBuffer = this._toBuffer();
+		return this.cachedBuffer;
+	}
+	_toBuffer() {
+		let buffer = new GrowableBuffer();
+		this.addToBuffer(buffer);
+		return buffer.toBuffer();
+	}
 }
 
 //Non-recursive type
 class AbsoluteType extends Type {}
 
 class NullType extends AbsoluteType {
-  static get _value() {
-    return 0x00;
-  }
+	static get _value() {
+		return 0x00;
+	}
 }
 
 //Integer type
 class IntegerType extends Type {}
 class ByteType extends IntegerType {
-  static get _value() {
-    return 0x01;
-  }
+	static get _value() {
+		return 0x01;
+	}
 }
 class ShortType extends IntegerType {
-  static get _value() {
-    return 0x02;
-  }
+	static get _value() {
+		return 0x02;
+	}
 }
 class IntType extends IntegerType {
-  static get _value() {
-    return 0x03;
-  }
+	static get _value() {
+		return 0x03;
+	}
 }
 class LongType extends IntegerType {
-  static get _value() {
-    return 0x04;
-  }
+	static get _value() {
+		return 0x04;
+	}
 }
 
 module.exports = {
-  ByteType,
-  ShortType,
-  IntType,
-  LongType
+	ByteType,
+	ShortType,
+	IntType,
+	LongType
 };
