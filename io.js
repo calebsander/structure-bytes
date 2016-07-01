aad1f60d366f6c065da6795ff0c715dd24a4dba0
Caleb Sander <caleb.sander@gmail.com>
2016-07-01 18:34:01 -0400

    Untested functions in io; removed test output files

--- io.js
+++ io.js
@@ -1,11 +1,29 @@
 const BufferStream = require(__dirname + '/lib/buffer-stream.js');
 const GrowableBuffer = require(__dirname + '/lib/growable-buffer.js');
 
-module.exports = {
-  writeTypeAndValue: (type, value, outStream) => {
-    new BufferStream(type.toBuffer()).pipe(outStream, {end: false});
-    let valueBuffer = new GrowableBuffer();
+function close() {
+  this.end();
+}
+
+const io = module.exports = {
+  writeType: (type, outStream) => {
+    return new BufferStream(type.toBuffer()).pipe(outStream).on('error', close).on('finish', function() {
+      this.emit('sb-written');
+    });
+  },
+  writeValue: (type, value, outStream) => {
+    const valueBuffer = new GrowableBuffer();
     type.writeValue(valueBuffer, value);
-    return new BufferStream(valueBuffer).pipe(outStream);
+    return new BufferStream(valueBuffer).pipe(outStream).on('error', close).on('finish', function() {
+      this.emit('sb-written');
+    });
+  },
+  writeTypeAndValue: (type, value, outStream) => {
+    const typeStream = new BufferStream(type.toBuffer());
+    typeStream.pipe(outStream, {end: false}).on('error', close);
+    typeStream.on('bs-written', () => { //can't listen for finish because it isn't called on a pipe without an end
+      io.writeValue(type, value, outStream);
+    });
+    return outStream;
   }
 };
--- test/io/type-and-value.js
+++ test/io/type-and-value.js
@@ -5,7 +5,7 @@ let type = new t.MapType(new t.StringType(), new t.UnsignedIntType());
 let value = new Map().set('abc', 123).set('def', 4560).set('—ݥ—', 7512112);
 let outStream = fs.createWriteStream(OUT_FILE);
 let ended = false;
-io.writeTypeAndValue(type, value, outStream).on('finish', () => {
+io.writeTypeAndValue(type, value, outStream).on('sb-written', () => {
   let result = fs.readFileSync(OUT_FILE);
   assert.assert(result.equals(Buffer.from([0x54, 0x41, 0x13, 0, 0, 0, 3, 0, 0, 0, 3, 0x61, 0x62, 0x63, 0, 0, 0, 0x7b, 0, 0, 0, 3, 0x64, 0x65, 0x66, 0, 0, 0x11, 0xd0, 0, 0, 0, 8, 0xe2, 0x80, 0x94, 0xdd, 0xa5, 0xe2, 0x80, 0x94, 0, 0x72, 0xa0, 0x30])));
   ended = true;
