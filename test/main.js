9ba634d44779e9bdbae4b37cc237ce425dca3a90
Caleb Sander <caleb.sander@gmail.com>
2016-07-19 14:36:54 -0400

    Tests use exported module rather than individual files

--- test/main.js
+++ test/main.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 
+const index = require(__dirname + '/../index.js');
 const assert = require(__dirname + '/../lib/assert.js');
 const BufferStream = require(__dirname + '/../lib/buffer-stream.js');
 const GrowableBuffer = require(__dirname + '/../lib/growable-buffer.js');
-const io = require(__dirname + '/../io.js');
-const r = require(__dirname + '/../read.js');
+const io = index;
+const r = index;
 const ReplaceStream = require(__dirname + '/../lib/replace-stream.js');
 const Simultaneity = require(__dirname + '/../lib/simultaneity.js');
-const t = require(__dirname + '/../structure-types.js');
+const t = index;
 const util = require(__dirname + '/../lib/util-inspect.js');
 
 let asyncErrors = 0;
