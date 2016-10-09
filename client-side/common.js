89b6ad8cbba5baf6c817906372f75562ebba9f9f
Caleb Sander <caleb.sander@gmail.com>
2016-10-09 00:24:28 -0400

    Fixed failing eslint

--- client-side/common.js
+++ client-side/common.js
@@ -9,6 +9,8 @@ require('/client-side/binary-ajax.js')
 if (!window.sb) {
 	window.sb = require('/structure-types.js')
 	const recursiveRegistry = require('/recursive-registry.js')
-	for (const key in recursiveRegistry) window.sb[key] = recursiveRegistry[key]
+	for (const key in recursiveRegistry) {
+		if ({}.hasOwnProperty.call(recursiveRegistry, key)) window.sb[key] = recursiveRegistry[key]
+	}
 }
 else if (!(window.sb instanceof Object)) throw new Error('window.sb is already defined')
