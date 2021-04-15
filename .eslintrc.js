214d98ccc8e8aa93a7db5acd1a41759700c8827e
Caleb Sander <caleb.sander@gmail.com>
2021-04-15 15:04:05 -0400

    Update dependencies and tslint -> eslint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module'
+	},
+	plugins: [
+		'@typescript-eslint'
+	],
+	rules: {
+		indent: [
+			'error',
+			'tab',
+			{
+				SwitchCase: 1,
+				flatTernaryExpressions: true
+			}
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		quotes: [
+			'error',
+			'single'
+		],
+		semi: [
+			'error',
+			'never'
+		],
+		'@typescript-eslint/no-empty-function': 'off'
+	}
+}
