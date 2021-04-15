cd9a627f5e77a54fe9fb5a53708f2c3c85c1520c
Caleb Sander <caleb.sander@gmail.com>
2021-04-15 18:21:23 -0400

    Disable Node.js versions without BigInt

--- test/test-common.ts
+++ test/test-common.ts
@@ -1,12 +1,6 @@
 import * as builtinAssert from 'assert'
 
-//Polyfill for Node.js 8
-export const assert = builtinAssert.strict || Object.assign(builtinAssert, {
-	equal: builtinAssert.strictEqual,
-	deepEqual: builtinAssert.deepStrictEqual,
-	notEqual: builtinAssert.notStrictEqual,
-	notDeepEqual: builtinAssert.notDeepStrictEqual
-})
+export const assert = builtinAssert.strict
 
 export function concat(buffers: Uint8Array[]): Uint8Array {
 	const result = new Uint8Array(buffers.reduce((a, {length}) => a + length, 0))
